2ee17bfa5fab96c45eef88b369a3917ddd947b30
Simon Cannon <simon.cannon@gmail.com>
2019-04-28 08:22:52 +0100

    Added some less

--- Money.WebApp/ClientApp/src/app/account/account-list.component.ts
+++ Money.WebApp/ClientApp/src/app/account/account-list.component.ts
@@ -1,21 +1,15 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { BankAccount } from './bank-account';
 import { AccountService } from './account.service';
 
 @Component({
     selector: 'app-account-list',
-    templateUrl: './account-list.component.html'
+    templateUrl: './account-list.component.html',
+    styleUrls: ['./account-list.component.less']
 })
 export class AccountListComponent implements OnInit {
 
     public accounts: BankAccount[];
-
-    //constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    //    http.get<BankAccount[]>(baseUrl + 'api/account/GetAll').subscribe(result => {
-    //        this.accounts = result;
-    //    }, error => console.error(error));
-    //}
 
     constructor(private accountService: AccountService) {
     }
